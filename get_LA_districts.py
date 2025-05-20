2e389a3bdaf12243deb859e4186bdc35a0d7b1eb
edwardarchaeology <edwardarchaeology@gmail.com>
2025-05-20 16:10:41 -0500

    added in flat file generation for the LA agencies

--- get_LA_districts.py
+++ get_LA_districts.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 
 # API key from FBI API public testing account
 API_KEY = "iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv"
@@ -15,5 +16,21 @@ if response.status_code == 200:
     with open("LA_districts.json", "w") as f:
         json.dump(data, f, indent=2)
     print("Saved response to LA_districts.json")
+    
+    flat_data = []
+    for parish, agencies in data.items():
+        for agency in agencies:
+            agency["parish"] = parish  # add the parish name as a column
+            flat_data.append(agency)
+
+    # Convert to DataFrame
+    df = pd.DataFrame(flat_data)
+
+    # Optional: convert nibrs_start_date to datetime
+    df["nibrs_start_date"] = pd.to_datetime(df["nibrs_start_date"], errors="coerce")
+    df.to_csv("LA_agencies_flat.csv", index=False)
+    print("Saved flat form of response to LA_agencies_flat.csv")
+    
+
 else:
     print(f"Request failed: {response.status_code} - {response.text}")
